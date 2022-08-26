238b93ec68bde018a782ee59df764964b0651839
Zo <pahyzoard@outlook.hu>
2022-08-26 11:27:54 +0200

    html,css, done, javascript 20%

--- script.js
+++ script.js
@@ -2,16 +2,39 @@ const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const mult = document.querySelector(".mult");
 const divide = document.querySelector(".divide");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const zero = document.querySelector(".zero");
 const dot = document.querySelector(".dot");
 const clear = document.querySelector(".clear");
-const equals = document.querySelector(".equals");
+const equals = document.querySelector(".equals-box");
+const display = document.querySelector(".display");
+
+const nums = document.querySelectorAll(".nums");
+const numbers = Array.from(nums);
+
+numbers.forEach((n) =>
+  n.addEventListener("click", function () {
+    display.innerHTML += n.innerHTML;
+  })
+);
+
+plus.addEventListener("click", () => {
+  display.innerHTML += " + ";
+});
+minus.addEventListener("click", () => {
+  display.innerHTML += " - ";
+});
+mult.addEventListener("click", () => {
+  display.innerHTML += " * ";
+});
+divide.addEventListener("click", () => {
+  display.innerHTML += " / ";
+});
+dot.addEventListener("click", () => {
+  display.innerHTML += ".";
+});
+equals.addEventListener("click", () => {
+  display.innerHTML += " =";
+});
+// Clear
+clear.addEventListener("click", () => {
+  display.innerHTML = "";
+});
