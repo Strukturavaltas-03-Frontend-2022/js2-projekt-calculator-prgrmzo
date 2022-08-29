32d3bd47795d5e0c995bb35974aa0e9bd03028f3
Zo <pahyzoard@outlook.hu>
2022-08-29 07:55:18 +0200

    most ennyit tudtam

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const display = document.querySelector(".display");
 const nums = document.querySelectorAll(".nums");
 const numbers = Array.from(nums);
 
+let calculations = 0;
+
 numbers.forEach((n) =>
   n.addEventListener("click", function () {
     display.innerHTML += n.innerHTML;
@@ -31,8 +33,49 @@ divide.addEventListener("click", () => {
 dot.addEventListener("click", () => {
   display.innerHTML += ".";
 });
+
+// A számolás függvénye
+const calc = () => {
+  const calcArr = display.innerHTML.split(" ");
+  while (calcArr.length > 2) {
+    let num1 = Number(calcArr[0]);
+    let operator = calcArr[1];
+    let num2 = Number(calcArr[2]);
+
+    switch (operator) {
+      case "+":
+        calculations = num1 + num2;
+        break;
+      case "-":
+        calculations = num1 - num2;
+        break;
+      case "*":
+        calculations = num1 * num2;
+        break;
+      case "/":
+        calculations = num1 / num2;
+        break;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      calcArr.shift();
+    }
+    calcArr.unshift(calculations);
+  }
+
+  if (
+    !Number.isNaN(calcArr[0]) &&
+    Number.isSafeInteger(calcArr[0]) &&
+    Number.isFinite(calcArr[0])
+  ) {
+    display.innerHTML = calcArr[0];
+  } else {
+    display.innerHTML = "ERROR";
+  }
+};
+//Equals
 equals.addEventListener("click", () => {
-  display.innerHTML += " =";
+  calc();
 });
 // Clear
 clear.addEventListener("click", () => {
